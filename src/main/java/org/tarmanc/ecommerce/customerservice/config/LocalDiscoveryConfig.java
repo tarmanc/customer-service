701ddcbc86c7fb6e2c450bf42d5078af32bad486
adyingwish <62962574+adyingwish@users.noreply.github.com>
2021-02-15 23:23:22 +0300

    Integrated Spring Cloud Config

--- src/main/java/org/tarmanc/ecommerce/customerservice/config/LocalDiscoveryConfig.java
+++ src/main/java/org/tarmanc/ecommerce/customerservice/config/LocalDiscoveryConfig.java
@@ -2,9 +2,7 @@ package org.tarmanc.ecommerce.customerservice.config;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Profile("local-discovery")
 @EnableDiscoveryClient
 @Configuration
 public class LocalDiscoveryConfig {
--- src/main/java/org/tarmanc/ecommerce/customerservice/controller/ItemController.java
+++ src/main/java/org/tarmanc/ecommerce/customerservice/controller/ItemController.java
@@ -20,16 +20,19 @@ public class ItemController {
 
     @GetMapping("/items")
     public ResponseEntity<List<ItemBase>> getAllAvailableItems() {
+        System.out.println("Items Working");
         return new ResponseEntity<>(customerService.getAllItems(), HttpStatus.ACCEPTED);
     }
 
     @GetMapping("/item/{id}")
     public ResponseEntity<Item> getItemById(@PathVariable() UUID id) {
+        System.out.println("ID Working");
         return new ResponseEntity<>(customerService.getItemById(id), HttpStatus.ACCEPTED);
     }
 
     @GetMapping("/search")
     public ResponseEntity<List<ItemBase>> searchItems(@RequestParam String name) {
+        System.out.println("Search Working");
         return new ResponseEntity<>(customerService.searchItems(name), HttpStatus.ACCEPTED);
     }
 
